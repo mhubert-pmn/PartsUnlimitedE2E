576e7719717f15b7d1891652dfc9966edd1a5d3b
mhubert <mhubert@la-pmn.org>
2022-03-15 10:20:56 +0100

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // TP dev
+        // second change
         [Key]
         public int CartItemId { get; set; }
 
